10c15e79c26461c94975d554659f3e36e38ac8b1
coltonkramer <77127051+coltonkramer@users.noreply.github.com>
2022-07-19 20:37:29 -0400

    styling completed

--- pages/components/projects.js
+++ pages/components/projects.js
@@ -5,7 +5,7 @@ import {
   ProjectsContainer,
   ProjectsBodyContainer,
   ProjectsChipStyle,
-  ProjectsChipStyleHover,
+
   ProjectsChipStyleLink,
 } from "./style.module.css";
 import projects from "../assets/projectsData";
@@ -31,21 +31,20 @@ const Projects = () => {
         </div>
         <div className={ProjectsBodyContainer}>
           <div className={ProjectsChipStyle}>
-            <div >
-              <Link href="../components/pokemon-project" className={ProjectsChipStyleLink}>
+              <Link href="../components/pokemon-project" >
                 <a>{buildImageContainer(projects[0])}</a>
               </Link>
-            </div>
-            <div className={ProjectsChipStyleHover}>
-              <p>Click here for more information</p>
-            </div>
           </div>
+          <div className={ProjectsChipStyle}>
           <Link href="../components/calculator-project">
             <a>{buildImageContainer(projects[1])}</a>
           </Link>
+          </div>
+          <div className={ProjectsChipStyle}>
           <Link href="../components/rivian-project">
             <a>{buildImageContainer(projects[2])}</a>
           </Link>
+          </div>
         </div>
       </div>
     </>
